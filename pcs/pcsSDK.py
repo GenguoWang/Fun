741af97af8d26c84283b60b0a828e196d9847ee1
Kingo <genguo.wgg@alibaba-inc.com>
2013-10-21 10:19:49 +0800

    upload file

--- pcs/pcsSDK.py
+++ pcs/pcsSDK.py
@@ -6,76 +6,6 @@ import random
 import string
 
 _BOUNDARY_CHARS = string.digits + string.ascii_letters
-
-def encode_multipart(fields, files, boundary=None):
-    r"""Encode dict of form fields and dict of files as multipart/form-data.
-    Return tuple of (body_string, headers_dict). Each value in files is a dict
-    with required keys 'filename' and 'content', and optional 'mimetype' (if
-    not specified, tries to guess mime type or uses 'application/octet-stream').
-
-    >>> body, headers = encode_multipart({'FIELD': 'VALUE'},
-    ...                                  {'FILE': {'filename': 'F.TXT', 'content': 'CONTENT'}},
-    ...                                  boundary='BOUNDARY')
-    >>> print('\n'.join(repr(l) for l in body.split('\r\n')))
-    '--BOUNDARY'
-    'Content-Disposition: form-data; name="FIELD"'
-    ''
-    'VALUE'
-    '--BOUNDARY'
-    'Content-Disposition: form-data; name="FILE"; filename="F.TXT"'
-    'Content-Type: text/plain'
-    ''
-    'CONTENT'
-    '--BOUNDARY--'
-    ''
-    >>> print(sorted(headers.items()))
-    [('Content-Length', '193'), ('Content-Type', 'multipart/form-data; boundary=BOUNDARY')]
-    >>> len(body)
-    193
-    """
-    def escape_quote(s):
-        return s.replace('"', '\\"')
-
-    if boundary is None:
-        boundary = ''.join(random.choice(_BOUNDARY_CHARS) for i in range(30))
-    lines = []
-
-    for name, value in fields.items():
-        lines.extend((
-            '--{0}'.format(boundary),
-            'Content-Disposition: form-data; name="{0}"'.format(escape_quote(name)),
-            '',
-            str(value),
-        ))
-
-    for name, value in files.items():
-        filename = value['filename']
-        if 'mimetype' in value:
-            mimetype = value['mimetype']
-        else:
-            mimetype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-        lines.extend((
-            '--{0}'.format(boundary),
-            'Content-Disposition: form-data; name="{0}"; filename="{1}"'.format(
-                    escape_quote(name), escape_quote(filename)),
-            'Content-Type: {0}'.format(mimetype),
-            '',
-            value['content'],
-        ))
-
-    lines.extend((
-        '--{0}--'.format(boundary),
-        '',
-    ))
-    body = '\r\n'.join(lines)
-
-    headers = {
-        'Content-Type': 'multipart/form-data; boundary={0}'.format(boundary),
-        'Content-Length': str(len(body)),
-    }
-
-    return (body, headers)
-
 gApiKey = "jPNKG9NIr8o2gl0qeidwGddx"
 gSecertKey = "8Xn4m6ir9Ab3xd3BHGwKkzYz54EVoL9U"
 gCallbackUrl = "http://sharemark.tk/baidu/handle.php"
@@ -149,6 +79,7 @@ def getFileList(path,by=None,order=None,limit=None):
     return json.loads(res)
 
 def uploadFile(path,filePath,ondup="newcopy"):
+    #path need to have file name
     if len(path)==0 or (not path[0] == '/'):
         path = gAppRoot + path
     url = "https://pcs.baidu.com/rest/2.0/pcs/file"
@@ -172,16 +103,16 @@ def httpRequest(url,method,reqData,postData={},filename=""):
     elif method=="get":
         req = urllib2.Request(url)
     elif method=="postfile":
-        pData,headers = encode_multipart({},{"file":{"mimetype":"application/octet-stream","filename":filename,"content":open(filename, "rb").read()}})
-        #boundary = "---------------------------wggkingowangboundary5211314"
-        #boundarybytes = "\r\n--" + boundary + "\r\n"
-        #headers["ContentType"] = "multipart/form-data; boundary=" + boundary
-        #pData = "Content-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\nContent-Type: application/octet-stream\r\n\r\n"%("file",filename)
-        #pData += pData+open(filename, "rb").read()
-        #pData += "\r\n--" + boundary + "--\r\n"
-        #headers['Content-Length'] = str(len(pData))
+        boundary = ''.join(random.choice(_BOUNDARY_CHARS) for i in range(30))
+        pData = "--"+boundary+"\r\nContent-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\nContent-Type: application/octet-stream\r\n\r\n"%("file",filename)
+        pData += open(filename, "rb").read()
+        pData += "\r\n--" + boundary + "--\r\n"
+        headers = {}
+        headers["Content-Type"] = "multipart/form-data; boundary=" + boundary
+        headers['Content-Length'] = str(len(pData))
         if gDebug:
-            print "post:" +pData
+            print pData
+            print headers
         req = urllib2.Request(url,pData,headers)
     else:
         raise ValueError
@@ -194,3 +125,5 @@ def httpRequest(url,method,reqData,postData={},filename=""):
         print res
     return res
 
+#if _NAME_ == "main":
+uploadFile("1.txt","1.txt")
